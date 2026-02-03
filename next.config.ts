518e431f4f4a7fadf5df2118820d5ae77fc36044
AbdulAmmic <connectorstech@gmail.com>
2026-02-03 12:49:05 +0100

    comit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://dataapi-connectorstech7925-t9vnrjmbf6zbpor48c.leapcell-async.dev/api/:path*",
+      },
+      {
+        source: "/webhooks/:path*",
+        destination: "https://dataapi-connectorstech7925-t9vnrjmbf6zbpor48c.leapcell-async.dev/webhooks/:path*",
+      },
+      {
+        source: "/health",
+        destination: "https://dataapi-connectorstech7925-t9vnrjmbf6zbpor48c.leapcell-async.dev/health",
+      }
+    ];
+  },
 };
 
 export default nextConfig;
